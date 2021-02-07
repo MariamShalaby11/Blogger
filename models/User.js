4311ff8f87cc1c362d74ab5cb09fe1c06853efb7
MariamShalaby <77920984+MariamShalaby11@users.noreply.github.com>
2021-02-07 16:38:54 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -21,9 +21,9 @@ const userSchema = new Schema({
     lastname:{
         type:String,
         required:true},
-    email:{
-        type:String,
-        required:true},
+    // email:{
+    //     type:String,
+    //     required:true},
      age:{type:Number,
         required:true},
     image:String,   
