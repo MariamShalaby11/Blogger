09604a6381f532edbf98c4f19c5c4dcb1b24984a
MariamShalaby <77920984+MariamShalaby11@users.noreply.github.com>
2021-02-08 13:57:40 +0200

    Update blog.js

--- controllers/blog.js
+++ controllers/blog.js
@@ -18,7 +18,8 @@ const getByTitle=(title) =>Blog.find({title}).exec();
 //search by tag
 const getByTag=(tags) =>Blog.find({tags}).exec();
 //write comment
-const comment = (id,body) =>Blog.findByIdAndUpdate( id ,{ $set: {comments:body} },{ new: true, useFindAndModify: false })
+const comment = (id,body) =>Blog.updateOne({_id:id},{$set:body},{new:true}).exec()
+//Blog.findByIdAndUpdate( id ,{ $set: {comments:body} },{ new: true, useFindAndModify: false })
 // findByIdAndUpdate( id, { $push: { comments:body} },{ new: true, useFindAndModify: false })
 //  Blog.updateOne({_id:id},{$set:body},{new:true}).exec()
 
