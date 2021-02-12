5750116a9d183926f2aa4dc9f85556f33ff9171d
MariamShalaby <77920984+MariamShalaby11@users.noreply.github.com>
2021-02-12 18:01:28 +0200

    ..

--- index.js
+++ index.js
@@ -16,11 +16,23 @@ mongoose.connect(MONGODB_DBURL, {
 
 
 const app=express();//isntance from express
+app.use(function (req, res, next) {​​
 
+    //Enabling CORS
+
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+
+    next();
+
+}​​);
 app.use(express.json());//middleware to read from body
-
 app.use(express.urlencoded({​​​​ extended: true }​​​​))
-app.use(cors());
+// app.use(cors());
+
 // router.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Methods: PUT,GET,POST,DELETE,PATCH");
 //      res.header("Access-Control-Allow-Origin: *");
